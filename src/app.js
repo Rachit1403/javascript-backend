e5e3a9844bcfe2430986a3eb38a92b5dd90a8b0e
Rachit Gupta <rachitgupta209@gmail.com>
2024-03-18 02:48:20 +0530

    added file upload utils and middleware. implemented CORS

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ import express from "express"
 
 const app = express();
 
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(cookieParser())
+
 export {app};
